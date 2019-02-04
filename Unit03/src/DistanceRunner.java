d4d7ce816b1c64c4e7685f45fb08ae9ba4acc455
Elizabeth Kwon <elizabeth.h.kwon@gmail.com>
2019-02-04 11:06:21 -0800

    distance and distance runner complete

---
+++ Unit03/src/DistanceRunner.java
@@ -0,0 +1,37 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class DistanceRunner
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(in);
+
+		out.print("Enter x1 :: ");
+		int x1 = keyboard.nextInt();
+
+		out.print("Enter y1 :: ");
+		int y1 = keyboard.nextInt();
+
+		out.print("Enter x2 :: ");
+		int x2 = keyboard.nextInt();
+		
+		out.print("Enter y2 :: ");
+		int y2 = keyboard.nextInt(); 
+		
+		Distance distance = new Distance(x1, y1, x2, y2);
+		distance.calcDistance();
+		distance.print();
+			
+			
+	}
+}
