954ea33bbc42778aec122142868e601cd522028b
Elizabeth Kwon <elizabeth.h.kwon@gmail.com>
2019-02-06 11:08:23 -0800

    Unit 4 Completed

--- Unit04/src/NumberVerify.java
+++ Unit04/src/NumberVerify.java
@@ -1,21 +1,34 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -  
-//Class -
-//Lab  -
+//Name - Elizabeth Kwon
+//Date - 02/06/2019
+//Class - AP CSA
+//Lab  - Odd or Even
 
 import static java.lang.System.*;
 
 public class NumberVerify
 {
-	public static boolean isOdd( int num )
+	public static boolean isOdd( int num ) //checking odd
 	{
-		return true;
+		if (num%2==1 || num%2==-1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
-	public static boolean isEven( int num )
-	{
+	public static boolean isEven( int num ) //checking even
+	{ 
+		if (num%2==0) {
 		return true;
+		}
+		else {
+			return false;
+		}
 	}	
+	
 }
+	
+	
--- Unit04/src/NumberVerifyRunner.java
+++ Unit04/src/NumberVerifyRunner.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Elizabeth Kwon
+//Date - 02/06/2019
+//Class - AP CSA
+//Lab  - Odd or Even
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -17,6 +17,10 @@ public class NumberVerifyRunner
 		System.out.println("5 is odd :: " + NumberVerify.isOdd(5));
 		System.out.println("5 is even :: " + NumberVerify.isEven(5));
 		
-		//add in more test cases
+		System.out.println("2000 is odd :: " + NumberVerify.isOdd(2000));
+		System.out.println("2000 is even :: " + NumberVerify.isEven(2000));
+		
+		System.out.println("-11 is odd :: " + NumberVerify.isOdd(-11));
+		System.out.println("-11 is even :: " + NumberVerify.isEven(-11));
 	}
 }
