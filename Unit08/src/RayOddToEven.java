99ca6267e08b249e8c298a6513a588cb66c8ce44
Elizabeth Kwon <kwonlizzy2@gmail.com>
2019-02-26 20:00:12 -0800

    update ray odd to even

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -7,6 +7,26 @@ public class RayOddToEven
 {
 	public static boolean go(int[] ray)
 	{
+		int oddspace = 0;
+		int firstodd = 0; 
+		int evenspace = 0;
+		int difference = 0; 
+		for (int i=0; i< ray.length; i++) {
+			if (i%2==1) {
+				oddspace = i; 
+				break; 
+			}
+		}
+		for (int j=oddspace; i< ray.length; i++) {
+			if (j%2==0) {
+				evenspace = j; 
+				break; 
+			}
+		}
+		difference = evenspace - oddspace; 
+		if (oddspace == 0) {
+			
+		}
 		return false;
 	}
 }
