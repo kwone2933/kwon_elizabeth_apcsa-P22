a24e0a47b88794c563723eb283563814085b3e0d
Elizabeth Kwon <elizabeth.h.kwon@gmail.com>
2019-03-14 11:07:06 -0700

    continue unit 11

--- Unit11/src/ToyRunner.java
+++ Unit11/src/ToyRunner.java
@@ -8,5 +8,7 @@ public class ToyRunner
 {
 	public static void main(String[] args)
 	{
+		Scanner keyboard = new Scanner(System.in);
+		
 	}
 }
--- Unit11/src/TriangleWordRunner.java
+++ Unit11/src/TriangleWordRunner.java
@@ -9,6 +9,17 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
-		
+		Scanner keyboard = new Scanner(System.in);
+		String choice = ""; 
+		do {
+			out.print("\n Enter a word: ");
+			String word = keyboard.next(); 
+			
+			TriangleWord.printTriangle(word);
+			
+			System.out.print("\nDo you want to enter more sample input? ");
+			choice=keyboard.next();			
+		}while(choice.equals("Y")||choice.equals("y"));	
+			
+		}
 	}
-}
