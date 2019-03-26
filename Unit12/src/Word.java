246d6856e0213274e53047942356871609e5177f
Elizabeth Kwon <elizabeth.h.kwon@gmail.com>
2019-03-26 09:56:18 -0700

    never pushed some 12 edits to github

--- Unit12/src/Word.java
+++ Unit12/src/Word.java
@@ -10,15 +10,46 @@ public class Word implements Comparable<Word>
 
 	public Word( String s )
 	{
+		word = s;
+	}
+	
+	public String getWord()
+	{
+		return word;
 	}
 
 	public int compareTo( Word rhs )
-	{		
+	{	
+		if (rhs.getWord().length() != word.length()) 
+	{
+		if (rhs.getWord().length() < word.length())
+		{
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {
+		char[] rhsArray = rhs.getWord().toCharArray();
+		char[] wordArray = word.toCharArray();
+		for (int i = 0; i < rhsArray.length; i++)
+		{
+			if (rhsArray[i] < wordArray[i]) 
+			{
+				return 1;
+			}
+			else if (rhsArray[i] > wordArray[i])
+			{
+				return -1;
+			}
+		}
 		return 0;
 	}
+}
 
 	public String toString()
 	{
-		return "";
+		return word;
 	}
 }
--- Unit12/src/WordRunner.java
+++ Unit12/src/WordRunner.java
@@ -15,18 +15,18 @@ public class WordRunner
 		Scanner file = new Scanner(new File("words.dat"));
 
 		int size = file.nextInt();
+		Word [] words = new Word[size];
 		file.nextLine();
+		for (int i = 0; i < size; i++) {
+			words[i] = new Word(file.nextLine());
+		}
 		
+		Arrays.sort(words);
+		for (Word s: words)
+		{
+			System.out.println(s);
+		}
 
-
-
-
-
-
-
-
-
-
-
+		file.close();
 	}
 }
